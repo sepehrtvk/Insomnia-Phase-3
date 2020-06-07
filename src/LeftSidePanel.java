c71e97a4f1e7e1af08681244ce049205e489e0f5
9831111 <9831111@ceit.local>
2020-06-07 16:16:23 +0430

    Fix list duplicate frame bug.

--- src/LeftSidePanel.java
+++ src/LeftSidePanel.java
@@ -63,28 +63,6 @@ public class LeftSidePanel extends JPanel {
         filterTextField.setColumns(10);
         add(filterTextField);
 
-        //add request lists.
-//        requestsTree = new JTree();
-//        requestsTree.setBounds(1, 87, 253, 491);
-//        requestsTree.setPreferredSize(new Dimension(100, 500));
-//        requestsTree.setBackground(Color.DARK_GRAY);
-//
-//        requestsTree.setModel(new DefaultTreeModel(new DefaultMutableTreeNode("Requests") {
-//            {
-//                DefaultMutableTreeNode node1, node2;
-//
-//                node1 = new DefaultMutableTreeNode();
-//                node1.add(new DefaultMutableTreeNode());
-//                node1.add(new DefaultMutableTreeNode());
-//                add(node1);
-//
-//                node2 = new DefaultMutableTreeNode();
-//                node2.add(new DefaultMutableTreeNode());
-//                add(node2);
-//            }
-//        }));
-
-//        add(requestsTree);
         JLabel jLabel = new JLabel("Requests : ");
         jLabel.setBounds(10, 90, 100, 20);
         add(jLabel);
@@ -97,66 +75,63 @@ public class LeftSidePanel extends JPanel {
         if (listOfFiles != null) {
             for (File file : listOfFiles) {
                 if (file.isFile() && !file.getName().equals(".DS_Store")) {
-                    System.out.println(file.getName());
                     l1.addElement(file.getName());
-
                 }
             }
         }
         list.addListSelectionListener(new ListSelectionListener() {
+            int counter=0;
             @Override
             public void valueChanged(ListSelectionEvent e) {
+                if(counter!=0)return;
                 System.out.println(list.getSelectedValue());
                 String[] args = new String[3];
                 args[0] = "-fire";
                 args[1] = list.getSelectedValue();
                 JFrame listFrame = new JFrame("Choose one request : ");
                 listFrame.setLayout(new BorderLayout());
-                listFrame.setBounds(50,50,300,100);
+                listFrame.setBounds(50, 50, 300, 200);
                 JTextArea jTextArea = new JTextArea();
                 jTextArea.setEditable(false);
                 try {
                     Scanner sc = new Scanner(new File("Requests/" + args[1]));
                     int i = 1;
                     while (sc.hasNext()) {
-                       // System.out.println(i + ": " + sc.nextLine());
-                        jTextArea.append(i + ": " + sc.nextLine()+"\n");
+                        jTextArea.append(i + ": " + sc.nextLine() + "\n");
                         i++;
                         sc.nextLine();
                     }
                 } catch (FileNotFoundException ex) {
                     ex.printStackTrace();
                 }
-                listFrame.add(jTextArea,BorderLayout.CENTER);
+                listFrame.add(jTextArea, BorderLayout.CENTER);
                 listFrame.setVisible(true);
                 JButton OKBtn = new JButton("OK");
-                listFrame.add(OKBtn,BorderLayout.SOUTH);
+                listFrame.add(OKBtn, BorderLayout.SOUTH);
                 JTextField jTextField = new JTextField();
-                listFrame.add(jTextField,BorderLayout.NORTH);
+                listFrame.add(jTextField, BorderLayout.NORTH);
                 OKBtn.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                    try {
+                        try {
 
-                        Scanner sc = new Scanner(new File("Requests/" + args[1]));
-                        for (int i = 1; i < Integer.parseInt(jTextField.getText()); i++) {
+                            Scanner sc = new Scanner(new File("Requests/" + args[1]));
+                            for (int i = 1; i < Integer.parseInt(jTextField.getText()); i++) {
+                                sc.nextLine();
+                                sc.nextLine();
+                            }
                             sc.nextLine();
-                            sc.nextLine();
+                            String[] arr = sc.nextLine().split("\\s+");
+                            arr[1] = "-i";
+                            Request request = new Request(arr);
+                            request.send();
+                        } catch (FileNotFoundException ex) {
+                            ex.printStackTrace();
                         }
-                        sc.nextLine();
-                        String[] arr = sc.nextLine().split("\\s+");
-                        for(String str  : arr){
-                            System.out.println(str);
-                        }
-                        arr[1]="-i";
-                        Request request = new Request(arr);
-                        request.send();
-                    } catch (FileNotFoundException ex) {
-                        ex.printStackTrace();
-                    }
                         listFrame.dispose();
                     }
                 });
+                counter++;
             }
 
         });
