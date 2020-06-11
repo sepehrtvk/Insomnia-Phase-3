52a25ecb2c20f0841480cc4f48512f12b5e8af19
9831111 <9831111@ceit.local>
2020-06-11 20:27:04 +0430

    Fix save and read.

--- src/SaveButtonAction.java
+++ src/SaveButtonAction.java
@@ -42,43 +42,90 @@ public class SaveButtonAction implements ActionListener {
         OKBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                argees.add(Controller.url.getText());
+                Component[] queryPanelComponents = Controller.qeuryPanel.getComponents();
+                JCheckBox jCheckBoxQuery = (JCheckBox) queryPanelComponents[5];
+                if (jCheckBoxQuery.isSelected()) {
+
+                    StringBuilder sb = new StringBuilder();
+
+                    for (int i = 3; i < queryPanelComponents.length; i = i + 5) {
+                        JTextField key = (JTextField) queryPanelComponents[i];
+                        JTextField value = (JTextField) queryPanelComponents[i + 1];
+                        JCheckBox jCheckBox2 = (JCheckBox) queryPanelComponents[i + 2];
+                        if (jCheckBox2.isSelected())
+                            sb.append(key.getText() + "=" + value.getText() + "&");
+
+                    }
+                    String str = sb.toString();
+                    sb.deleteCharAt(str.length() - 1);
+                    argees.add(Controller.url.getText() + "?" + sb.toString());
+                } else argees.add(Controller.url.getText());
+
+
                 argees.add("-M");
                 argees.add(Controller.methodsComboBox.getSelectedItem().toString());
                 argees.add("-i");
-                argees.add("-S");
-                argees.add(jTextField.getText());
+                if (Controller.followRedirect.isSelected()) argees.add("-f");
+
+
+                Component[] headerPanelComponents = Controller.headerPanel.getComponents();
+                JCheckBox jCheckBox = (JCheckBox) headerPanelComponents[3];
+                if (jCheckBox.isSelected()) {
+                    argees.add("--headers");
+                    StringBuilder sb = new StringBuilder();
+
+                    for (int i = 1; i < headerPanelComponents.length; i = i + 5) {
+                        JTextField key = (JTextField) headerPanelComponents[i];
+                        JTextField value = (JTextField) headerPanelComponents[i + 1];
+                        JCheckBox jCheckBox2 = (JCheckBox) headerPanelComponents[i + 2];
+                        if (jCheckBox2.isSelected())
+                            sb.append(key.getText() + ":" + value.getText() + ";");
+
+                    }
+                    String str = sb.toString();
+                    sb.deleteCharAt(str.length() - 1);
+                    argees.add(sb.toString());
+                }
+                if (Controller.formData) {
+                    Component[] bodyNewPanelComponents = Controller.bodyNewPanel.getComponents();
+                    JCheckBox jCheckBox2 = (JCheckBox) bodyNewPanelComponents[3];
+                    if (jCheckBox2.isSelected()) {
+                        argees.add("-d");
+                        StringBuilder sb = new StringBuilder();
+
+                        for (int i = 1; i < bodyNewPanelComponents.length; i = i + 5) {
+                            JTextField key = (JTextField) bodyNewPanelComponents[i];
+                            JTextField value = (JTextField) bodyNewPanelComponents[i + 1];
+                            JCheckBox jCheckBox3 = (JCheckBox) bodyNewPanelComponents[i + 2];
+                            if (jCheckBox3.isSelected())
+                                sb.append(key.getText() + "=" + value.getText() + "&");
+
+                        }
+                        String str = sb.toString();
+                        sb.deleteCharAt(str.length() - 1);
+                        argees.add(sb.toString());
+                    }
+                }
+                if (!Controller.uploadFile.equals("")) {
+                    argees.add("-u");
+                    argees.add(Controller.uploadFile);
+                }
+
                 String[] arr = new String[argees.size()];
                 arr = argees.toArray(arr);
+                Request request = new Request(arr);
 
-
+                String command = "";
                 for (String str : arr) {
-                    System.out.println(str);
+                    command += str + " ";
                 }
                 File file = new File("Requests/" + jTextField.getText());
 
                 try {
-                    Request request = new Request(arr);
                     //write on the file.
                     FileWriter fr = new FileWriter(file, true);
                     fr.write(request.toString() + "\n");
-                    String command = Controller.url.getText() + " -M " +
-                            Controller.methodsComboBox.getSelectedItem().toString() +
-                            (Controller.followRedirect.isSelected()?" -f ":"");
-                    String headers = " -H ";
-                    for(int i = 1;i < Controller.headerPanel.getComponents().length;i+=5)
-                    {
-                        JTextField key = (JTextField) Controller.headerPanel.getComponents()[i];
-                        JTextField value = (JTextField) Controller.headerPanel.getComponents()[i + 1];
-                        JCheckBox jCheckBox2 = (JCheckBox) Controller.headerPanel.getComponents()[i + 2];
-                        if (jCheckBox2.isSelected())
-                            headers += key.getText() + ":" + value.getText() + ";";
-                    }
-                    headers = headers.substring(0,headers.length()-1);
-                    if(!headers.equals(" -H"))
-                    command += headers;
-
-                    fr.write(command);
+                    fr.write(command + "\n");
                     fr.close();
                 } catch (IOException ee) {
                     ee.printStackTrace();
